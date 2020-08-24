6c4eec4a40c4adf2d8c0f3e6daaa0dc25e6e99dc
FrancineyAndrews <69489893+FrancineyAndrews@users.noreply.github.com>
2020-08-24 16:35:45 -0300

    Add files via upload

---
+++ Questao2b.py
@@ -0,0 +1,8 @@
+a = int(input("Digite o primeiro número: "))
+b = int(input("Digite o segundo número: "))
+c = int(input("Digite o terceiro número: "))
+média = (a+b+c)/3
+print("A média do valor é: {:.1f}".format(média))
+
+                    
+      
